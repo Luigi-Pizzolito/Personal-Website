deaff4f76a8be484b338a0be7757d1155e3318cf
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2020-03-08 21:14:52 +0800

    added JS for GET IG thumb loader

---
+++ assets/js/home.js
@@ -0,0 +1,17 @@
+/*
+ *  Contact Section
+*/
+// Instagram Listing
+IGhref = [];
+IGthumb = [];
+$.getJSON("https://www.instagram.com/luigi.pizzolito/?__a=1", function(jsonIG) {
+    for (let i=0; i < 12; i++) {
+        console.log(i);
+        let href = "http://instagram.com/p/"+jsonIG.graphql.user.edge_owner_to_timeline_media.edges[i].node.shortcode+"/";
+        IGhref.push(href);
+        console.log(href);
+        let thumb = jsonIG.graphql.user.edge_owner_to_timeline_media.edges[i].node.thumbnail_src;
+        IGthumb.push(thumb);
+        console.log(thumb);
+    }
+});
