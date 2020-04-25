a1345e12cdcb22c44cef20b0729d4fcd00d430f8
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2020-04-25 14:43:18 +0800

    Added LazyLoading (Asynchronous) for software

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,6 +7,105 @@ function mobileNav() {
 }
 
 /*
+ * Software Section
+ */
+// GHnames = [];
+// var GHcount = 0;
+// var links = [];
+function* shuffle(array) {
+        var i = array.length;
+        while (i--) {
+                yield array.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+        }
+}
+$.getJSON("https://api.github.com/users/Luigi-Pizzolito/repos", function (jsonGH) {
+        var ranNums = shuffle([...Array(jsonGH.length).keys()]);
+        for (let i = 0; i < jsonGH.length; i++) { //needs to be asynchronous for loading time improvement
+                let ran = ranNums.next().value;
+                let href = jsonGH[ran].full_name;
+                // GHnames.push(href);
+                var link = document.createElement("a");
+                link.href = "https://github.com/" + href;
+                var img = document.createElement("img");
+                // var img = new Image();
+                img.type = "image/svg+xml";
+                // img.src = "https://gh-card.dev/repos/" + href + ".svg?link_target=_top";
+                img.setAttribute("data-src", "https://gh-card.dev/repos/" + href + ".svg?link_target=_top");
+                img.classList.add("lazy")
+                // img.onloaad = function () {
+                //         document.querySelectorAll(".bus-in")[0].appendChild(this);
+                //         console.log("added child")
+                // };
+                // img.src = "data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+                // img.setAttribute("data-src", "https://gh-card.dev/repos/" + href + ".svg?link_target=_top");
+                // lazyloadimage(img, "https://gh-card.dev/repos/" + href + ".svg?link_target=_top");
+                link.appendChild(img);
+                let div = document.createElement('div');
+                div.classList.add("item");
+                div.appendChild(link);
+                document.querySelectorAll(".bus-in")[0].appendChild(div);
+                // links.push(div);
+                // setTimeout(() => {
+                // document.querySelectorAll(".bus-in")[0].appendChild(links[GHcount++]);
+                // console.log(links[GHcount++]);
+
+                // }, 50 + i * 50, i);
+                // console.log(href)
+        }
+
+        function logElementEvent(eventName, element) {
+                console.log(Date.now(), eventName, element.getAttribute("data-src"));
+        }
+        var callback_error = function (element) {
+                logElementEvent("💀 ERROR", element);
+                // element.src = "https://via.placeholder.com/440x560/?text=Error+Placeholder";
+                element.parentElement.parentElement.removeChild(element.parentElement.parentElement.lastChild)
+        };
+        var callback_finish = function () {
+                logElementEvent("✔️ FINISHED", document.documentElement);
+                //start scrollspy
+        };
+        var lazyLoadInstance = new LazyLoad({
+                elements_selector: ".lazy",
+                threshold: 0,
+                callback_error: callback_error,
+                callback_finish: callback_finish
+                // ... more custom settings?
+        });
+        //     lazyLoadInstance.update();
+        //lazy load
+        // for (var i = 0; i < document.getElementsByClassName("bus-in")[0].getElementsByTagName("img").length; i++) {
+        //         var image = document.getElementsByClassName("bus-in")[0].getElementsByTagName("img")[i];
+        //         var downloadingImage = new Image();
+        //         downloadingImage.onload = function () {
+        //                 console.log("loaded "+this.src);
+        //                 console.log(image);
+        //                 image.removeAttribute("src")
+        //                 image.src = this.src;
+        //                 image.removeAttribute("data-src");
+        //                 image.classList.add("loaded");
+        //                 console.log(image);
+
+        //                 //experimental
+        //                 // document.getElementsByClassName("bus-in")[0].getElementsByTagName("img")[i].parentElement.removeChild(document.getElementsByClassName("bus-in")[0].getElementsByTagName("img")[0].parentElement.lastChild);
+        //                 document.getElementsByClassName("bus-in")[0].getElementsByTagName("a")[i].appendChild(image)
+
+        //         };
+        //         downloadingImage.src = image.dataset.src;
+        // }
+});
+
+// function lazyloadimage(imagetagin, imageurl) {
+//         var image = imagetagin;
+//         image.src = "data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+//         var downloadingImage = new Image();
+//         downloadingImage.onload = function () {
+//                 image.src = this.src;
+//         };
+//         downloadingImage.src = imageurl;
+// }
+
+/*
  *  Contact Section
  */
 // Instagram Listing
