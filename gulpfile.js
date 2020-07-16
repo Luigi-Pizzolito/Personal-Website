e14e27a157f21e4448ef5515b8c0d912aee669cd
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2020-07-17 04:16:16 +0800

    Testing directories for Jekyll Categories

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task('jade', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['sass']); //, '_includes/**/*'
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '*.yml', 'assets/img/**', 'assets/js/**.js', 'assets/img/**.*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_posts/**',  'projects/*', 'projects/**', '*.yml', 'assets/img/**', 'assets/js/**.js', 'assets/img/**.*'], ['jekyll-rebuild']);
     gulp.watch('_jadefiles/**/*.jade', ['jade']);
 });
 
