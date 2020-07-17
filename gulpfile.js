4fffd16a12f80149b7e8c41cca52ee4aecdf5201
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2020-07-17 15:52:07 +0800

    cleaned CSS for baseline blog pages

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,15 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('assets/css'));
+    gulp.src('assets/css/baseline.scss')
+        .pipe(sass({
+            includePaths: ['css'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
     return gulp.src('assets/css/main.scss')
         .pipe(sass({
             includePaths: ['css'],
