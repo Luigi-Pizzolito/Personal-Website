713714baeff0dea473db1cde12a351fbaccdcf2f
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2020-12-27 03:36:35 +0800

    Finished JS for multi image gallery
    CSS still needs fixing

--- _includes/blog/galery-multimodal.js
+++ _includes/blog/galery-multimodal.js
@@ -1,3 +1,117 @@
-function createGaleryMultiModals() {
-    console.log("hi")
+function createGaleryMultiModal(modal) {
+    // var modal = document.querySelector('.multi-img-modal');
+    var images = modal.getElementsByTagName('img');
+    var modalImgs = document.querySelector('.modal-imgs');
+    var modalDots = document.querySelector('.modal-dots');
+
+    //clear images in modal
+    modalDots.innerHTML = "";
+    modalImgs.innerHTML = "";
+
+    //populate images in modal
+    for (var i = 0; i < images.length; i++) {
+        LazyLoad.load(images[i]);
+
+        var addimg = document.createElement('div');
+        var img = images[i].cloneNode();
+        // var img = document.createElement('img');
+        img.src = images[i].src;
+        // img.alt = images[i].alt;
+        // img.classList = images[i].classList;
+        // img.dataset.src = images[i].dataset.src;
+        // img.dataset.llStatus = images[i].dataset.llStatus;
+        img.classList.add('image-slide')
+        img.removeAttribute('onclick')
+        addimg.classList.add('slide');
+        addimg.appendChild(img);
+        modalImgs.appendChild(addimg);
+
+        var addimg2 = document.createElement('div');
+        var img2 = images[i].cloneNode();
+        // var img2 = document.createElement('img');
+        img2.src = images[i].src;
+        // img2.alt = images[i].alt;
+        // img2.classList = images[i].classList;
+        // img2.dataset.src = images[i].dataset.src;
+        // img2.dataset.llStatus = images[i].dataset.llStatus;
+        img2.classList.add('modal-preview')
+        img2.classList.add('hover-shadow')
+        img2.removeAttribute('onclick')
+        addimg2.classList.add('col');
+        addimg2.setAttribute('onclick', 'toSlide('+(i+1)+');');
+        addimg2.appendChild(img2);
+        modalDots.appendChild(addimg2)
+    }
+
+    // currentModal = modal;
+    // slideIndex = sliden;
+
+    //temp
+    // showSlide(slideIndex);
 }
+
+function setupModalClicks() {
+    var modals = document.querySelectorAll('.multi-img-modal');
+    for (var i = 0; i < modals.length; i++) {
+        var imgs = modals[i].getElementsByTagName('img');
+        for (var b = 0; b < imgs.length; b++) {
+            imgs[b].setAttribute('onclick', "createGaleryMultiModal(this.parentElement);openLightbox();toSlide("+(b+1)+");");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+let slideIndex = 1;
+// var currentModal;
+// showSlide(slideIndex);
+
+function openLightbox() {
+    document.getElementById('lightbox').style.display = 'block';
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+};
+
+// Note that you are assigning new values here to our slidIndex.
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+};
+
+function toSlide(n) {
+    showSlide(slideIndex = n);
+};
+
+
+function showSlide(n) {
+    let scope = document.querySelector('#lightbox .modal-content');
+    const slides = scope.getElementsByClassName('slide');
+    let modalPreviews = scope.getElementsByClassName('modal-preview');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    };
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    };
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    };
+
+    for (let i = 0; i < modalPreviews.length; i++) {
+        modalPreviews[i].classList.remove('active');
+    };
+
+    slides[slideIndex - 1].style.display = 'block';
+    modalPreviews[slideIndex - 1].classList.add('active');
+};
--- projects/hardware/_posts/2020-04-20-welcome-to-jekyll.md
+++ projects/hardware/_posts/2020-04-20-welcome-to-jekyll.md
@@ -11,5 +11,20 @@ Image load test:
 <!-- ![Miku Forever](/assets/img/blog/83606.jpg) -->
 <!-- ![Miku Forever](as){:class="lazy" data-src="/assets/img/blog/83606.jpg"} -->
 <!-- <img src="/assets/img/blog/83606.jpg" alt="Miku Forever"> -->
-<img alt="A lazy image" class="lazy" data-src="/assets/img/blog/83606.jpg" />
-<img alt="lazylaoded" class="lazy" data-src="https://wallpaperplay.com/walls/full/9/a/c/35089.jpg" />
+
+
+
+<div class="multi-img-modal">
+    <img alt="A lazy image" class="lazy" data-src="/assets/img/blog/83606.jpg" />
+    <img alt="lazylaoded" class="lazy" data-src="https://wallpaperplay.com/walls/full/9/a/c/35089.jpg" />
+    <img alt="A lazy image" class="lazy" data-src="https://content.instructables.com/ORIG/F1F/47D3/JDUKGFSQ/F1F47D3JDUKGFSQ.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=57f37a99def0aae541036a975fb4485e" />
+    <img alt="lazylaoded" class="lazy" data-src="https://wallpaperplay.com/walls/full/9/a/c/35089.jpg" />
+</div>
+
+<div class="multi-img-modal">
+    <img alt="oswin" class="lazy" data-src="https://tse2-mm.cn.bing.net/th/id/OIP.jcEAATnXbZcdZ246Uy46PQHaI9?pid=Api&rs=1" />
+    <img alt="oswin" class="lazy" data-src="http://www.thegeekgeneration.com/wp-content/uploads/2012/09/Oswin-Oswald-communicator.jpg" />
+    <img alt="oswin" class="lazy" data-src="https://tse2-mm.cn.bing.net/th/id/OIP.RSjdsii6J6M3Va1zeY_WAgHaEE?pid=Api&rs=1" />
+    <img alt="oswin" class="lazy" data-src="https://tse4-mm.cn.bing.net/th/id/OIP.7ZWAOn9QJqZ9V7QFfsEzdQHaEb?pid=Api&rs=1" />
+    <img alt="oswin" class="lazy" data-src="http://images6.fanpop.com/image/photos/33900000/Oswin-doctor-who-33981138-494-611.jpg" />
+</div>
